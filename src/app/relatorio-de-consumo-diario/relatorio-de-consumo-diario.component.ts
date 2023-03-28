bf466d5b5e8c9c50b28a4446c5eb1999bd17bb27
GARSoftPoli <garsoftestudos@gmail.com>
2023-03-28 02:15:31 -0300

    Implements ngIf view for when there's no response

--- src/app/relatorio-de-consumo-diario/relatorio-de-consumo-diario.component.ts
+++ src/app/relatorio-de-consumo-diario/relatorio-de-consumo-diario.component.ts
@@ -11,6 +11,7 @@ export class RelatorioDeConsumoDiarioComponent{
   response : any = {} as any;
   date = new FormControl(new Date());
   maxDate = new Date();
+  theresResponse : number = 1;
 
   constructor(private apiClient : ApiService){
     //Separates the date in day, month and year to make a formatted string
@@ -47,21 +48,29 @@ export class RelatorioDeConsumoDiarioComponent{
   async getData(dateString: string){
     //Gets the response using the ApiService
     this.response = await this.apiClient.getResponse(dateString, "consumo");
-
-    //Sets the chart's divs innerHTML
-    let consumo_acumulado = document.getElementById("chartConsumoAcumulado");
-    let curva_carga = document.getElementById("chartCurvaDeCarga");
-    curva_carga!.innerHTML = this.response["curva-de-carga"];
-    consumo_acumulado!.innerHTML = this.response["consumo-acumulado"];
-
-    let scriptTags = curva_carga!.getElementsByTagName('script');
-    for (let i = 0; i < scriptTags.length; i++) {
-      eval(scriptTags[i].innerHTML);
+    if(Object.keys(this.response).length === 0){
+      this.theresResponse = 0;
+      return;
     }
+    else this.theresResponse = 1;
 
-    scriptTags = consumo_acumulado!.getElementsByTagName('script');
-    for (let i = 0; i < scriptTags.length; i++) {
-      eval(scriptTags[i].innerHTML);
-    }
+    setTimeout(() => { //Waits for 5ms to make sure the ngIf has changed the view
+      //Sets the chart's divs innerHTML
+      let consumo_acumulado = document.getElementById("chartConsumoAcumulado");
+      let curva_carga = document.getElementById("chartCurvaDeCarga");
+      
+      curva_carga!.innerHTML = this.response["curva-de-carga"];
+      consumo_acumulado!.innerHTML = this.response["consumo-acumulado"];
+
+      let scriptTags = curva_carga!.getElementsByTagName('script');
+      for (let i = 0; i < scriptTags.length; i++) {
+        eval(scriptTags[i].innerHTML);
+      }
+
+      scriptTags = consumo_acumulado!.getElementsByTagName('script');
+      for (let i = 0; i < scriptTags.length; i++) {
+        eval(scriptTags[i].innerHTML);
+      }
+    }, 5);
   }
 }
