e84312cb2cea767384ef27951770978da024d4e8
Gabriel Palassi <gabrielpalassi@hotmail.com>
2023-03-28 15:49:09 -0300

    Mocks side-bar demanda atual

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ApiService } from '../services/ApiService';
 
 @Component({
   selector: 'app-side-bar',
@@ -8,34 +7,22 @@ import { ApiService } from '../services/ApiService';
 })
 
 export class SideBarComponent {
-  today = new Date();
-  response : any = {} as any;
+  demandaAtual = 702;
 
-  constructor(private apiClient : ApiService){
-    this.getData(); //Starts getData loop
+  constructor(){
+    this.generateRandomNumbers();
   }
-
-  async getData() {
-    while (true){ //Loops every 15 minutes
-      //Sets today again in case the day has changed
-      this.today = new Date();
-
-      //Separates the date in day, month and year to make a formatted string
-      var day = this.today.getDate();
-      var month = this.today.getMonth() + 1; //For some reason month goes from 0 to 11
-      var year = this.today.getFullYear();
-
-      //Handles the date so that dateString is always YYYY-MM-DD
-      if(day <= 9 && month <= 9) dateString = year + "-" + "0" + month + "-" + "0" + day;
-      else if(day <= 9) var dateString = year + "-" + month + "-" + "0" + day;
-      else if(month <= 9) var dateString = year + "-" + "0" + month + "-" + day;
-      else var dateString = year + "-" + month + "-" + day;
-
-      //Gets the response
-      this.response = await this.apiClient.getResponse(dateString, "consumo");
-
-      //Waits for 15 minutes
-      await new Promise(resolve => setTimeout(resolve, 900000));
+  
+  async generateRandomNumbers() {
+    while(true){
+      await new Promise<void>(resolve => {
+        setTimeout(() => {
+          // Generate a random number in the range of 600 to 800
+          this.demandaAtual = Math.floor(Math.random() * (800 - 600 + 1)) + 600;
+          resolve();
+        }, 10000); // Delay the execution of each iteration by 10 seconds (10000 milliseconds)
+      });
     }
   }
+  
 }
