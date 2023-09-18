23d653a891d8be76f0ad0ebc709b23d51884c5c6
Gabriel Palassi <gabrielpalassi@hotmail.com>
2023-09-18 15:15:29 -0300

    #1 chore: updates version/name across the project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Energy Digital Twin
+# EnergyView
 
 This project was created with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.4 as a product of GARSoft's ownership, do not copy or redistribute this code without granted permission.
 
@@ -8,6 +8,6 @@ Note that this code requires Angular to work, so be sure to install it.
 
 1. Clone the repository on a local directory;
 2. Inside the directory open a terminal;
-3. Run `ng serve --open`.
+3. Run `npm run start`.
 
 A browser window should open when the project is done compiling.
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'EnergyDigitalTwin'`, () => {
+  it(`should have as title 'EnergyView'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('EnergyDigitalTwin');
+    expect(app.title).toEqual('EnergyView');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('EnergyDigitalTwin app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('EnergyView app is running!');
   });
 });
